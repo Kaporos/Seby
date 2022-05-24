83deff5a5da1c7d2ccb0a2bcd88acb93bbae1654
Kaporos <theo@daron.be>
2022-05-24 17:47:06 +0200

    Adding style

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,15 @@ import { render } from 'preact'
 import { App } from './app'
 import './index.css'
 
-let container = document.createElement("div");
-container.id = "seby"
-document.body.appendChild(container)
+fetch("https://raw.githubusercontent.com/Kaporos/Seby/main/dist/index.css").then(r => r.text()).then((style) => {
+    let elem = document.createElement("style")
+    elem.innerText = style+="\n"
+    document.body.appendChild(elem)
+    let container = document.createElement("div");
+    container.id = "seby"
+    document.body.appendChild(container)
 
-render(<App/>, container)
+    render(<App/>, container)
+})
+
+
