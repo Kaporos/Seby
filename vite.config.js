78e8e17c628f89fe3b739e5bec409450fc5c48c1
Kaporos <theo@daron.be>
2022-05-24 17:15:42 +0200

    Base project

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,19 @@ import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()]
+  plugins: [preact()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "index.js",
+        assetFileNames: (assetInfo) => {
+          let split = assetInfo.name.split("/")
+          if (assetInfo.name == 'style.css')
+            return 'style.css';
+          return split[split.length - 1]
+        },
+      }
+    }
+
+  }
 })
